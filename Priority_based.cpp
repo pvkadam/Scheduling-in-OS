3de7771a216b9196958457b8c733536497cb2c0a
pvkadam <poojakadam8396@gmail.com>
2017-12-26 15:31:12 -0800

    Sorting added

--- Priority_based.cpp
+++ Priority_based.cpp
@@ -30,3 +30,31 @@ class Priority
         p[i]=i+1;           //contains process number
     }
 }
+void sortPriority()
+	{
+		//sorting burst time, priority and process number in ascending order using selection sort,least number ll hv more priority
+    for( i=0;i<n;i++)
+    {
+        pos=i;
+        for( j=i+1;j<n;j++)
+        {
+            if(pr[j]<pr[pos])
+                pos=j;
+        }
+ 
+        temp=pr[i];
+        pr[i]=pr[pos];
+        pr[pos]=temp;
+    
+ 	
+        temp=bt[i];
+        bt[i]=bt[pos];
+        bt[pos]=temp;
+ 
+        temp=p[i];
+        p[i]=p[pos];
+        p[pos]=temp;
+    }
+	}
+	friend void waittime(Priority);
+};
