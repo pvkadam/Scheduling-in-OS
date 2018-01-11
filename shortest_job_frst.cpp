89a5a941961b137f4ce87186fef346f0638fce6f
pvkadam <poojakadam8396@gmail.com>
2018-01-10 23:53:14 -0800

    Create shortest_job_frst.cpp

---
+++ shortest_job_frst.cpp
@@ -0,0 +1,14 @@
+ #include <iostream>
+    using namespace std;
+
+    int main()
+    {
+	int n, burst[20], arrival[20], throughput;
+    {
+      cout << "Output for SJF_Non_Preemptive scheduling algorithm" << endl;
+
+      int i, j, temp, temp2;
+      double tot, avgwait, avgturnaround, avgresponse, tp;
+
+      //array instantiations                                                                                          
+      int start[n], end[n], wait[n];
