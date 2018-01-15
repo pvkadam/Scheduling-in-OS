a865800a73dbb51e499f1d08df501f5a2cad2611
pvkadam <poojakadam8396@gmail.com>
2018-01-15 15:07:43 -0800

    Update shortest_job_frst.cpp

--- shortest_job_frst.cpp
+++ shortest_job_frst.cpp
@@ -1,73 +1,26 @@
-#include <iostream>
-    using namespace std;
+#include<iostream>
+ using namespace std;
+ 
 
-    int main()
+int main()
+{    int n=3;
+    float total,wait[3]={0};
+    float p[3],twaiting=0,waiting=0;
+    int proc;
+    int stack[3];
+    float brust[3],arrival[3],sbrust,temp[3],top=3;
+
+for(int i=0;i<n;i++)
     {
-	int n, burst[20], arrival[20], throughput;
-    {
-      cout << "Output for SJF_Non_Preemptive scheduling algorithm" << endl;
-
-      int i, j, temp, temp2;
-      double tot, avgwait, avgturnaround, avgresponse, tp;
-
-      //array instantiations                                                                                          
-      int start[n], end[n], wait[n];
-
-      //calculations                                                                                                  
-      for(i=1;i<=n;i++)
-        {
-          for(j=i+1;j<=n;j++)
-            {
-              if (i>=2 && burst[i-1]>burst[j-1])
-                {
-                  temp = burst[i-1];
-                  temp2 = arrival[i-1];
-                  burst[i-1]=burst[j-1];
-                  arrival[i-1]=arrival[j-1];
-                  burst[j-1]=temp;
-                  arrival[j-1]=temp2;
-                }
-            }
-          if(i==1)
-            {
-              start[0]=0;
-              end[0]=burst[0];
-              wait[0]=0;
-            }
-          else
-            {
-              start[i-1]=end[i-2];
-              end[i-1]=start[i-1]+burst[i-1];
-              wait[i-1]=start[i-1]+arrival[i-1];
-            }
-          //throughput                                                                                                
-          if (start[i+1] <= throughput)
-            tp = i+1;
-        }
-
-      //output                                                                                                        
-      cout << "\n\nPROCESS \t BURST TIME\tARRIVAL TIME\tWAIT TIME\tSTART TIME\tEND TIME\n";
-      for (i=0;i<n;i++){
-        cout << "\nP[" << i + 1 << "]" << "\t\t" << burst[i] << "\t\t" << arrival[i] << "\t\t" << wait[i] << "\t\t" << start[i] << "\t\t" << end[i];
-      }
-      //avg wait time                                                                                                 
-      for(i=1,tot=0;i<n;i++){
-        tot+=wait[i-1];
-        avgwait=tot/n;
-      }
-      //avg turnaround time                                                                                           
-      for(i=1,tot=0;i<n;i++){
-        tot+=end[i-1];
-        avgturnaround=tot/n;
-      }
-      //avg response time                                                                                             
-      for(i=1,tot=0;i<n;i++){
-        tot+=start[i-1];
-        avgresponse=tot/n;
-      }
-      cout << "\n\nAverage Wait Time: " << avgwait;
-      cout << "\nAverage Response Time: " << avgturnaround;
-      cout << "\nAverage Turnaround Time: " << avgresponse;
-      cout << "\nThroughput for (" << throughput << "): " << tp << endl;
+        p[i]=i;
+        stack[i]=i;
+        cout<<"enter arival time :";
+        cin>>arrival[i];
+        cout<<endl<<"enter brust time:";
+        cin>>brust[i];
+        temp[i]=arrival[i];
+        sbrust=brust[i]+sbrust;
+        
+        
     }
-}
+    
