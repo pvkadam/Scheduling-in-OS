a22b12242b1297e7bdd539afdc12b9c529ab72ce
pvkadam <poojakadam8396@gmail.com>
2018-01-19 19:50:25 -0800

    Update srt.cpp

--- srt.cpp
+++ srt.cpp
@@ -79,3 +79,18 @@ void Srtsched::dispTime()
     cout<<"Avg Waiting time = "<<(double)twt/n<<" and Avg Turnaround time = "<<(double)ttat/n<<endl;
     cout<<"Scheduling complete\n";
 }
+
+
+int Srtsched::getNextProcess(int time){
+ 
+        int i,low;
+        for(i=0;i<n;i++)
+            if(finish[i]==0){low=i; break; }
+        for(i=0;i<n;i++)
+            if(finish[i]!=1)
+                if(rt[i]<rt[low] && at[i]<=time)
+                        low=i;
+        return low;
+ 
+}
+ 
